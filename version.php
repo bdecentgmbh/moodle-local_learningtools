fdf10ba4225fb55eb38c2513a20e7314e7a049eb
bdecent <hello@bdecent.de>
2021-08-28 10:19:57 +0200

    Update version.php
    Bump version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_learningtools';
-$plugin->version = 2021082500;
+$plugin->version = 2021082501;
 $plugin->release = 'v1.0';
 $plugin->requires = 2020061501;
 $plugin->maturity = MATURITY_STABLE;
