f892ca84c9b36a00f832b9f883e25a12823f6a3c
Stefan-Alexander Scholz <hello@bdecent.de>
2024-12-12 12:09:01 +0100

    Update version.php

--- version.php
+++ version.php
@@ -25,8 +25,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_learningtools';
 $plugin->version = 2024120600;
-$plugin->release = 'v1.1.1';
+$plugin->release = 'v1.2';
 $plugin->requires = 2022041900; // Moodle 4.0.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [400, 405];
-
